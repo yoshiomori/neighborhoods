b6f8bf10ad5e06e4ed78612bd75552a2e45592bf
Yoshio Mori <yoshio.mori@hotmail.com>
2013-08-28 17:43:32 -0300

    Criando estrutura do Alphabeto

---
+++ alphabet.c
@@ -0,0 +1,13 @@
+#include "alphabet.h"
+#include <stdio.h>
+
+static char **alphabet = NULL;
+static int length_alphabet = 0;
+
+void insert_alphabet(char * character){
+  if(alphabet){
+    
+  }
+  else{
+  }
+}
